8907be8b9343f6bc7bfbeb7e62e99aa79bb4f34f
Min Chou <mchou9@asu.edu>
2023-09-23 15:14:10 +0800

    initial setup

---
+++ jasmine.js
@@ -0,0 +1,59 @@
+// JavaScript source code
+
+	<!--DO NOT FORGET THIS SCRIPT TAG SO YOU CAN USE JQUERY!!!!!-- >
+
+		function TestButtonHandler() {
+			var webMethod = "ProjectServices.asmx/TestConnection";
+			var parameters = "{}";
+
+			//jQuery ajax method
+			$.ajax({
+				type: "POST",
+				url: webMethod,
+				data: parameters,
+				contentType: "application/json; charset=utf-8",
+				dataType: "json",
+				success: function (msg) {
+					var responseFromServer = msg.d;
+					alert(responseFromServer);
+				},
+				error: function (e) {
+					alert("this code will only execute if javascript is unable to access the webservice");
+				}
+			});
+		}
+
+		function logon() {
+			var id = document.getElementById("logonId").value;
+			var pass = document.getElementById("logonPass").value;
+			var webMethod = "ProjectServices.asmx/LogOn";
+			//var parameters =  "{\"uid\":\"" + encodeURI(id) + "\",\"pass\":\"" + encodeURI(pass) + "\"} ";
+		var param = {
+			uid: encodeURI(id),
+		pass: encodeURI(pass)
+            }
+		var parameters = JSON.stringify(param);
+
+		//jQuery ajax method
+		$.ajax({
+			type: "POST",
+		url: webMethod,
+		data: parameters,
+		contentType: "application/json; charset=utf-8",
+		dataType: "json",
+		success: function (msg) {
+					var responseFromServer = msg.d;
+		if (responseFromServer == true) {
+			alert("logged in!")
+		}
+		else {
+			alert("bad credential!")
+		}		
+                },
+		error: function (e) {
+			alert("this code will only execute if javascript is unable to access the webservice");
+                }
+            });
+		}
+
+	<!--END OF YOUR OWN JAVASCRIPT-- >
